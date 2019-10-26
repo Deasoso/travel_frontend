e79f9d5c6089b09bdb4e7a4e27fd994005bd57fd
Deasoso <932152571@qq.com>
2019-10-26 15:07:12 +0800

    add aaa.

--- src/pages/Index/index.vue
+++ src/pages/Index/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="demo-page">
-    abc
+    aaa
     <h3 class="title"><span>欢迎打开{{title}}</span></h3>
     <button class="btn" @click="routeDetail">跳转到TodoMVC</button>
   </div>
